1f6eeb4b39c99bd506cb7931d32de9fc59a9fedb
Alvaro <alvaro.fernandez@iesnervion.es>
2022-02-21 21:54:19 +0100

    Initial commit

---
+++ src/Clases/Producto.java
@@ -0,0 +1,47 @@
+package Clases;
+
+public class Producto {
+    private int id;
+    private String descripcion;
+    private float precio;
+    private int stock;
+
+    public Producto(int id, String descripcion, float precio, int stock) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
